9ea8842d4326df9a4210cf1e6afbec580aee9526
Rebecca ONeill <rebecca.e.oneill3@gmail.com>
2024-08-15 14:51:14 +0100

    rails watch list without styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,19 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
   # Defines the root path route ("/")
   # root "posts#index"
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list
+
+  # get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: :bookmark
+  # post 'lists/:id/bookmarks', to: 'bookmarks#create'
 end
